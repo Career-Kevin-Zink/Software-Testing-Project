f59d33d5cd53cc067448587b2f00bde9bfa59203
Joshua Colicchio <JoshuaColicchio@users.noreply.github.com>
2021-03-08 20:01:52 -0500

    Update userCreationTest.java
    Added really sad comments, but I don't want to lose points because I didn't add obvious comments.

--- test/java/userCreationTest.java
+++ test/java/userCreationTest.java
@@ -6,11 +6,13 @@ class userCreationTest {
 
     @Test
     public void testSubmitUserDetails() {
+        // The simulated inputs
         String firstName = "John";
         String lastName = "John";
         String username = "jdoe09";
         String password = "12345678";
 
+        // The simulated inputs should pass the following:
         assertTrue(!firstName.isEmpty()
                             && !lastName.isEmpty()
                             && username.length() > 3

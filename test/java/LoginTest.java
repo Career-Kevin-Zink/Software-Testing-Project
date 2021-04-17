a9b1ad33413c46d6673c68c030e1870759cb589f
Peter Look <pjlookii2220@eagle.fgcu.edu>
2021-04-17 18:52:15 -0400

    Correcting misc.xml and build.gradle
    We had java 1.8 set in misc.xml, and in build.gradle we had Mockito dependencies commented out. Other than that just some import optimizations and adding the misc.xml to the gitignore.

--- test/java/LoginTest.java
+++ test/java/LoginTest.java
@@ -1,10 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class LoginTest {
     /**

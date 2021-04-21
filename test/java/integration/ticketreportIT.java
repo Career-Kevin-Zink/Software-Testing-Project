9e9bede6c66850f72159207e4e52d211334b1ea8
Zinkmk <43687915+Zinkmk@users.noreply.github.com>
2021-04-21 00:17:03 -0400

    performance Testing
    Added performance tests to the test suites

--- test/java/integration/ticketreportIT.java
+++ test/java/integration/ticketreportIT.java
@@ -1,5 +1,7 @@
 package integration;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 
 import static org.mockito.Mockito.when;
@@ -7,25 +9,51 @@ import app.ticket;
 import app.Main;
 import app.ticketreport;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @RunWith(MockitoJUnitRunner.class)
 public class ticketreportIT {
-  @Mock
-  Main mainMock;
-  @Mock
-  ticketreport ticketreportMock;
-
-  @Test
-  @DisplayName("Verify JTable values not null")
-  public void checkJtable(){
-    ticketreportMock = mock(ticketreport.class);
-
-
-
-  }
 }
+//  @Mock
+//  private Connection conn;
+//  @Mock
+//  private ResultSet rs;
+//  @Mock
+//  private PreparedStatement pst;
+//
+//  @BeforeAll
+//  private void setupMockDB(){
+//
+//    try{
+//      when(conn.prepareStatement(any(String.class))).thenReturn(pst);
+//      when(rs.first()).thenReturn(true);
+//
+//
+//    }catch (SQLException se) {
+//      se.printStackTrace();
+//      System.out.println("SQL Exception in ticketreportIT: setUpMockDB");
+//    }
+//
+//  }
+//
+//  @Test
+//  @DisplayName("Verify JTable values not null")
+//  public void checkJtable(){
+//    ticketreport TR = new ticketreport();
+//    TR.jButton1.doClick();
+//    when(TR.jButton1.doClick()).thenReturn(true);
+////assertTrue(when(conn == any(String.class))).thenReturn(true);
+
+  //}
+//}
--- test/java/performance/ticketPT.java
+++ test/java/performance/ticketPT.java
@@ -1,20 +1,173 @@
 package performance;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import app.ticket;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ticketPT {
+  long startTime = java.util.Calendar.getInstance().getTimeInMillis();
+  long endTime = java.util.Calendar.getInstance().getTimeInMillis();
+
+  @BeforeEach
+  @AfterEach
+  public void initTicketTable() {
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+      Connection con = DriverManager.getConnection("jdbc:mysql://localhost/airline","root","");
+      Statement s = con.createStatement();
+      s.execute("DROP TABLE `ticket`");
+      s.execute("CREATE TABLE `ticket` (" +
+              "  `id` varchar(255) NOT NULL," +
+              "  `flightid` varchar(255) NOT NULL," +
+              "  `custid` varchar(255) NOT NULL," +
+              "  `class` varchar(255) NOT NULL," +
+              "  `price` varchar(255) NOT NULL," +
+              "  `seats` varchar(255) NOT NULL," +
+              "  `date` varchar(255) NOT NULL" +
+              ") ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+      s.execute("INSERT INTO `ticket` (`id`, `flightid`, `custid`, `class`, `price`, `seats`, `date`) VALUES\n" +
+              "('TO001', 'FO003', 'CS001', 'Economy', 9000, 1, '2019-06-15')," +
+              "('TO002', 'FO003', 'CS001', 'Economy', 9000, 2, '2019-06-15')," +
+              "('TO003', 'FO001', 'CS003', 'Economy', 50000, 3, '2019-07-01');");
+
+      // add table
+    } catch (SQLException | ClassNotFoundException ignored){}
+  }
+  public void wipeTicketTable() {
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+      Connection con = DriverManager.getConnection("jdbc:mysql://localhost/airline","root","");
+      Statement s = con.createStatement();
+      s.execute("DROP TABLE `ticket`");
+      s.execute("CREATE TABLE `ticket` (\n" +
+              "  `id` varchar(255) NOT NULL,\n" +
+              "  `flightid` varchar(255) NOT NULL,\n" +
+              "  `custid` varchar(255) NOT NULL,\n" +
+              "  `class` varchar(255) NOT NULL,\n" +
+              "  `price` varchar(255) NOT NULL,\n" +
+              "  `seats` varchar(255) NOT NULL,\n" +
+              "  `date` varchar(255) NOT NULL\n" +
+              ") ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+    }catch (SQLException | ClassNotFoundException ignored) {}
+  }
+
+
   @Test
   @DisplayName("Initialize ticket UI in less than 5 seconds")
   void initComponents() {
-    long startTime = java.util.Calendar.getInstance().getTimeInMillis();
+    startTime = java.util.Calendar.getInstance().getTimeInMillis();
     new ticket();
-    long endTime = java.util.Calendar.getInstance().getTimeInMillis();
-
+    endTime = java.util.Calendar.getInstance().getTimeInMillis();
     assertTrue(endTime - startTime <= 5000);
   }
 
+  @Test
+  void testJButton3ActionPerformed() {
+    startTime = java.util.Calendar.getInstance().getTimeInMillis();
+    ticket ticket = new ticket();
+
+    //Simulating input into dropdown box's
+    ticket.txtsource.setSelectedItem("India");
+    ticket.txtdepart.setSelectedItem("Uk");
+    // Executing the method
+    ticket.jButton3.doClick();
+    //Testing to see if JTable is filled
+    endTime = java.util.Calendar.getInstance().getTimeInMillis();
+    assertTrue(endTime - startTime <= 5000);
+  }
+  @Test
+  void testJButton4ActionPerformed(){
+    startTime = java.util.Calendar.getInstance().getTimeInMillis();
+    ticket ticket = new ticket();
+
+    ticket.txtcustid.setText("83748902");
+    ticket.jButton4.doClick();
+    ticket.txtcustid.setText("CS001");
+    ticket.jButton4.doClick();
+    endTime = java.util.Calendar.getInstance().getTimeInMillis();
+    assertTrue(endTime - startTime <= 5000);
+
+  }
+  @Test
+  void testJTable1MouseClicked() {
+    startTime = java.util.Calendar.getInstance().getTimeInMillis();
+    ticket ticket = new ticket();
+    ticket.txtsource.setSelectedItem("India");
+    ticket.txtdepart.setSelectedItem("Uk");
+    // Executing the method
+    ticket.jButton3.doClick();
+
+    ticket.jTable1.changeSelection(0,0,false,false);
+
+    DefaultTableModel Df = (DefaultTableModel) ticket.jTable1.getModel();
+    int selectIndex = ticket.jTable1.getSelectedRow();
+
+    ticket.flightno.setText(Df.getValueAt(selectIndex, 0).toString());
+    ticket.flightname.setText(Df.getValueAt(selectIndex, 1).toString());
+    ticket.txtdept.setText(Df.getValueAt(selectIndex, 5).toString());
+    ticket.txtprice.setText(Df.getValueAt(selectIndex, 7).toString());
+    endTime = java.util.Calendar.getInstance().getTimeInMillis();
+    assertTrue(endTime - startTime <= 5000);
+  }
+  @Test
+  void testTxtseatsStateChanged(){
+    startTime = java.util.Calendar.getInstance().getTimeInMillis();
+    ticket ticket = new ticket();
+    ticket.txtprice.setText("2000");
+    ticket.txtseats.setValue(2);
+    endTime = java.util.Calendar.getInstance().getTimeInMillis();
+    assertTrue(endTime - startTime <= 5000);
+  }
+  @Test
+  void testJButton1ActionPerformed(){
+    startTime = java.util.Calendar.getInstance().getTimeInMillis();
+    ticket ticket = new ticket();
+
+    ticket.txtticketno.setText("TO004");
+    ticket.flightno.setText("FO001");
+    ticket.txtcustid.setText("CS001");
+    ticket.txtclass.setSelectedItem("Economy");
+    ticket.txtprice.setText("50000");
+    ticket.txtseats.setValue(1);
+
+    ticket.jButton1.doClick();
+    endTime = java.util.Calendar.getInstance().getTimeInMillis();
+    assertTrue(endTime - startTime <= 5000);
+  }
+  @Test
+  void testJButton2ActionPerformed(){
+    startTime = java.util.Calendar.getInstance().getTimeInMillis();
+    ticket ticket = new ticket();
+
+    ticket.jButton2.doClick();
+    endTime = java.util.Calendar.getInstance().getTimeInMillis();
+    assertTrue(endTime - startTime <= 5000);
+  }
+  @Test
+  void testAutoID() {
+    // Test for "MAX(id)") != null.
+    startTime = java.util.Calendar.getInstance().getTimeInMillis();
+    assertDoesNotThrow(() -> {
+      new ticket().autoID();
+    });
+
+    // Clear the database to trigger other branch.
+    wipeTicketTable();
+    // Test for "MAX(id)") == null.
+    assertDoesNotThrow(() -> {
+      new ticket().autoID();
+
+    });
+    endTime = java.util.Calendar.getInstance().getTimeInMillis();
+    assertTrue(endTime - startTime <= 5000);
+  }
 }
